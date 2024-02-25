b2597448f1285b9a486edc627511c86282e7f369
bhargav066 <bhargav066>
2024-02-25 06:52:34 +0000

    updated code

---
+++ Data_Ingestion.py
@@ -0,0 +1,60 @@
+# Databricks notebook source
+def file_exists_dbfs(file_path):
+    try:
+        dbutils.fs.ls(file_path)
+        return True
+    except Exception as e:
+        return False
+
+# COMMAND ----------
+
+def load_data_to_delta(file_path, file_type, load_type, database_name, table_name):
+
+    spark.sql(f"CREATE DATABASE IF NOT EXISTS {database_name}")
+    
+    spark.catalog.setCurrentDatabase(database_name)
+
+    if not file_exists_dbfs(file_path):
+        raise FileNotFoundError(f"No file found at {file_path}")
+
+    if load_type == "incremental":
+        pass  # Implement incremental load logic here
+    else:
+        if file_type == "csv":
+            df = spark.read.option("header", "true").csv(file_path)
+        elif file_type == "excel":
+            df = spark.read.format("com.crealytics.spark.excel").option("header", "true").load(file_path)
+        elif file_type == "json":
+            df = spark.read.option("multiline", "true").json(file_path)
+        else:
+            raise ValueError(f"Unsupported file type: {file_type}")
+
+        for col in df.columns:
+            new_col = col.replace(" ", "_").replace(";", "").replace("{", "").replace("}", "").replace("(", "").replace(")", "").replace("\n", "").replace("\t", "").replace("=", "").replace("-", "_").lower()
+            df = df.withColumnRenamed(col, new_col)
+
+        # Write to Delta table
+        df.write.format("delta").mode("overwrite").saveAsTable(f"{database_name}.{table_name}")
+
+
+
+# COMMAND ----------
+
+load_data_to_delta("/FileStore/tables/Product.csv", "csv", "full","sales", "product_raw")
+
+# COMMAND ----------
+
+load_data_to_delta("/FileStore/tables/Customer.xlsx", "excel", "full","sales", "customer_raw")
+
+# COMMAND ----------
+
+load_data_to_delta("/FileStore/tables/Order.json", "json", "full","sales", "order_raw")
+
+# COMMAND ----------
+
+import os
+os.getcwd()
+
+# COMMAND ----------
+
+
